625e29f3b5be188a49550bcb6cc9399325d6da9d
JY <1776555345@qq.com>
2020-01-02 15:43:17 +0800

    解决layui上传文件太大造成网页崩溃（上传前回显造成）

--- src/main/resources/static/plugins/layuiadmin/modules/file_add.js
+++ src/main/resources/static/plugins/layuiadmin/modules/file_add.js
@@ -63,8 +63,55 @@ layui.use(["layer","form", "upload","jquery","element","formSelects"],function()
             this.data.param=JSON.stringify(map);
         }
         ,choose: function(obj){
-            var files = this.files = obj.pushFile(); //将每次选择的文件追加到文件队列
-            //读取本地文件
+            let files = this.files = obj.pushFile(); //将每次选择的文件追加到文件队列
+            let keys = Object.keys(files);
+
+            demoListView.empty();
+            for (let index of keys){
+                var tr = $([
+                    '<tr id="upload-'+ index +'">'+
+                        '<td><input type="text" class="layui-input" id="filenickname'+index+'" value="'+files[index].name+'"/></td>'+
+                        '<td>' +
+                            '<select name="filesortId'+index+'" xm-select="filesortId'+index+'" xm-select-search="" xm-select-radio>' +
+                                '<option value="">请选择文件分类</option>' +
+                            '</select>' +
+                        '</td>'+
+                        '<td>'+ ((files[index].size/1014)/1014).toFixed(1) +'MB</td>'+
+                        '<td>' +
+                            '<div class="layui-progress layui-progress-big" lay-showpercent="true" lay-filter="uploadProgressBar'+index+'">' +
+                                '<div class="layui-progress-bar" lay-percent="0%"></div>' +
+                            '</div>' +
+                        '</td>'+
+                        '<td>'+
+                            '<button class="layui-btn layui-btn-xs demo-reload layui-hide">重传</button>'+
+                            '<button class="layui-btn layui-btn-xs layui-btn-danger demo-delete">删除</button>'+
+                        '</td>'+
+                    '</tr>'
+                ].join(''));
+
+                //单个重传
+                tr.find('.demo-reload').on('click', function(){
+                    obj.upload(index, file);
+                });
+
+                //删除
+                tr.find('.demo-delete').on('click', function(){
+                    delete files[index]; //删除对应的文件
+                    tr.remove();
+                    uploadListIns.config.elem.next()[0].value = ''; //清空 input file 值，以免删除后出现同名文件不可选
+                });
+
+                file_names += files[index].name+",";
+                file_indexs += index+",";
+
+                demoListView.append(tr);
+                element.render('progress');//进度条div追加后，重新渲染进度条
+                formSelects.render('filesortId'+index);//重载formSelects
+                formSelects.data('filesortId'+index, 'local', {//给文件分类下拉赋值
+                    arr: fileSorts
+                });
+            }
+            /*//读取本地文件1577942454993-0
             obj.preview(function(index, file, result){
                 var tr = $([
                     '<tr id="upload-'+ index +'">'+
@@ -108,7 +155,7 @@ layui.use(["layer","form", "upload","jquery","element","formSelects"],function()
                 formSelects.data('filesortId'+index, 'local', {//给文件分类下拉赋值
                     arr: fileSorts
                 });
-            });
+            });*/
         }
         ,done: function(res, index, upload){
             if(res.code == '200'){ //上传成功
