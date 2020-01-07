19d34cd1408e4044125feef92d42706d2284d9de
JY <1776555345@qq.com>
2020-01-07 15:26:44 +0800

    解决文章封面图片不能上传BUG

--- src/main/resources/static/plugins/layuiadmin/modules/article_edit.js
+++ src/main/resources/static/plugins/layuiadmin/modules/article_edit.js
@@ -184,9 +184,27 @@ layui.define(['element', 'form', 'layer', 'upload', 'formSelects', 'inputTags'],
         }
     });
 
+    //创建监听函数
+    var xhrOnProgress=function(fun) {
+        xhrOnProgress.onprogress = fun; //绑定监听
+        //使用闭包实现监听绑
+        return function() {
+            //通过$.ajaxSettings.xhr();获得XMLHttpRequest对象
+            var xhr = $.ajaxSettings.xhr();
+            //判断监听函数是否为函数
+            if (typeof xhrOnProgress.onprogress !== 'function')
+                return xhr;
+            //如果有监听函数并且xhr对象支持绑定时就把监听函数绑定上去
+            if (xhrOnProgress.onprogress && xhr.upload) {
+                xhr.upload.onprogress = xhrOnProgress.onprogress;
+            }
+            return xhr;
+        }
+    };
     upload.render({
         elem: '#coverImg' //绑定元素
         , url: '/management/upload?reqType=lay' //上传接口
+        ,xhr:xhrOnProgress
         , error: function () {
             layer.msg("上传失败！");
         }
